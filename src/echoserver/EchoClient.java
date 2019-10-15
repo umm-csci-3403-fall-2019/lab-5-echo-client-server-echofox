45406a42735ca382ba1bf625028ac3a0dc77238e
UtkarshKr007 <kumar375@morris.umn.edu>
2019-10-15 11:29:49 -0500

    Document the client.java and server.java files and check that server_and_client bats test passes.
    Co-authored-by: AaronCorpstein <corps002@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,7 +8,8 @@ public class EchoClient {
 
     public static void main(String[] args) throws IOException{
         String server;
-
+        
+        // Make localhost the ip if no arguments are given
         if (args.length == 0){
             server = "127.0.0.1";
         } else {
@@ -19,19 +20,25 @@ public class EchoClient {
             // Connect to the server
             Socket socket = new Socket(server, portNumber);
 
+            // Socket streams to read from and write to
             InputStream input = socket.getInputStream();
             OutputStream output = socket.getOutputStream();
 
             int byteTyped;
 
+            // While it's reading bytes from the keyboard,
+            //  write to server and print the input echoed back
             while((byteTyped = System.in.read()) != -1){
                 output.write(byteTyped);
                 System.out.write(input.read());
             }
 
+            // Flush stops the system from grouping bytes together
+            // and close the socket
             System.out.flush();
             socket.close();
         
+            // Error handlers
         } catch (ConnectException ce) {
             System.out.println("We were unable to connect to " + server);
             System.out.println("You should make sure the server is running.");
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -17,20 +17,22 @@ public class EchoServer {
             Socket client = sock.accept();
             System.out.println("Got a request!");
 
+            // Input and Output streams to read from and write to client
             InputStream input = client.getInputStream();
             OutputStream output = client.getOutputStream();
 
             int byteRead;
 
-
+            // While it's reading bytes from the client,
+            // write the byte back to the client
             while((byteRead = input.read()) != -1){
                 output.write(byteRead);
             }
-            
+
             // Close the client socket since we're done.
             client.close();
           }
-        // *Very* minimal error handling.
+        //  Error handlers
         } catch (IOException ioe) {
           System.out.println("We caught an unexpected exception");
           System.err.println(ioe);
